6f4c58162fb643e4ab0fbc8ec44922b70e03d39b
oscar <oscar@claimstrak.co.uk>
2023-09-14 17:04:34 +0545

    Lesson 9: Form Fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 // layouts and pages
 import RootLayout from './layouts/RootLayout';
 import Dashboard, { tasksLoader } from './pages/Dashboard';
-import Create from './pages/Create';
+import Create, { createAction } from './pages/Create';
 import Profile from './pages/Profile';
 
 // router and routes
@@ -11,7 +16,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route index element={<Dashboard />} loader={tasksLoader} />
-      <Route path='create' element={<Create />} />
+      <Route path='create' element={<Create />} action={createAction} />
       <Route path='profile' element={<Profile />} />
     </Route>
   )
